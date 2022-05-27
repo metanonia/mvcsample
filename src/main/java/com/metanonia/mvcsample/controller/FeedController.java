a745bf2d7dd33be4e42e5ad5eea33056b0234701
Metanonia <metanonia@outlook.kr>
2022-05-27 15:17:21 +0900

    @AuthenticationPricipal 추가.

--- src/main/java/com/metanonia/mvcsample/controller/FeedController.java
+++ src/main/java/com/metanonia/mvcsample/controller/FeedController.java
@@ -1,14 +1,19 @@
 package com.metanonia.mvcsample.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/feed")
 public class FeedController {
     @GetMapping("/order")
-    public String orderPage() {
+    public String orderPage(@AuthenticationPrincipal UserDetails user) {
+        log.info(user.toString());
         return "/feed/order";
     }
 }
